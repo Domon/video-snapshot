49931bcc495e3d114a1daf9e7ad2d9aa58650365
Chun-wei Kuo <Dendoh@gmail.com>
2022-06-04 22:54:37 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Video Snapshot
+
+Display a snapshot of a YouTube video with `Cmd-Opt-S`. Click anywhere to dismiss.
+
+## License
+
+MIT
---
+++ video-snapshot.js
@@ -0,0 +1,25 @@
+var snapshot = function(e) {
+  if (!(e.metaKey && e.altKey && e.code == 'KeyS')) return;
+
+  var video = document.querySelector('video.html5-main-video');
+
+  var canvas = document.createElement('canvas');
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  var img = document.createElement('img');
+  img.src = canvas.toDataURL('image/webp');
+  img.alt = document.title + ' - ' + video.currentTime;
+
+  var overlay = document.createElement('div');
+  overlay.style.cssText = 'display: block; position: fixed; width: 100%; height: 100%; top:0; left: 0; right: 0, bottom: 0; background-color: rgba(0,0,0,0.75);';
+  overlay.onclick = function() {
+    overlay.remove();
+  };
+
+  overlay.appendChild(img);
+  document.body.appendChild(overlay);
+};
+
+document.addEventListener('keydown', snapshot);
