11c52063c5dee18383fbddcafd2cee613c445803
Chun-wei Kuo <Dendoh@gmail.com>
2022-06-06 11:04:04 +1000

    Center the image horizontally and vertically in the overlay

--- video-snapshot.js
+++ video-snapshot.js
@@ -8,15 +8,16 @@ var snapshot = function(e) {
   canvas.height = video.videoHeight;
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  var img = document.createElement('img');
-  img.src = canvas.toDataURL('image/webp');
-  img.alt = document.title + ' - ' + video.currentTime;
-
   var overlay = document.createElement('div');
-  overlay.style.cssText = 'display: block; position: fixed; width: 100%; height: 100%; top:0; left: 0; right: 0, bottom: 0; background-color: rgba(0,0,0,0.75);';
+  overlay.style.cssText = 'display: flex; justify-content: center; align-item: center; position: fixed; width: 100%; height: 100%; top:0; left: 0; right: 0, bottom: 0; background-color: rgba(0, 0, 0, 0.75);';
   overlay.onclick = function() {
     overlay.remove();
   };
+
+  var img = document.createElement('img');
+  img.style.cssText = 'margin: auto; width: 75%;';
+  img.src = canvas.toDataURL('image/webp');
+  img.alt = document.title + ' - ' + video.currentTime;
 
   overlay.appendChild(img);
   document.body.appendChild(overlay);
